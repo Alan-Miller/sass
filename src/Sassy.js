e765c9cb31696dff4d834b8d7b7bb42a20f1b52a
Alan-Miller <alan.ashman@gmail.com>
2017-09-12 11:46:12 -0600

    all files

---
+++ src/Sassy.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function SassSection(props) {
+  
+  return (
+    <div className="Sassy__main">
+      main section
+
+      <div className="box-1"></div>
+      <div className="box-2"></div>
+      <div className="box-3"></div>
+    </div>
+  )
+}
