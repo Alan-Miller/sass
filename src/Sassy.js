4bbed05e0a2eea803c8f70e5c5b81232fa1faed1
Alan-Miller <alan.ashman@gmail.com>
2018-03-06 10:01:43 -0700

    fixed login

--- src/Sassy.js
+++ src/Sassy.js
@@ -4,11 +4,10 @@ export default function SassSection(props) {
   
   return (
     <div className="Sassy__main">
-      main section
-
       <div className="box-1"></div>
       <div className="box-2"></div>
       <div className="box-3"></div>
+      <div className="box-4"></div>
     </div>
   )
 }
